7d800c8f8fb47e5e676f7f8abcaa0df512da1f29
mahmoud bouattay <mahmoudbouattay178@gmail.com>
2024-12-11 23:06:07 +0100

    first commit

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./home.css"
 import OfferEmploi from '../offerEmploi/offerEmploi';
 import Saidbar from '../../components/saidBar/saidbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 const Home = () => {
+    const { connected, msg, user } = useSelector((state) => state.login);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(connected!=true){
+            navigate("/login")
+        }
+        
+    }, []);
     return (
         <div className='home_container'>
             <Saidbar/>
